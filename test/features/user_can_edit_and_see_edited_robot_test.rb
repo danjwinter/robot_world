d792c87fd347dee92d15129de4b7b2ba1b9e11bd
Dan Winter <dan.j.winter@gmail.com>
2015-12-02 21:32:55 -0700

    added feature test for editing robots

---
+++ test/features/user_can_edit_and_see_edited_robot_test.rb
@@ -0,0 +1,40 @@
+require_relative '../test_helper'
+
+class UserCanSeeAndDeleteRobots < FeatureTest
+
+  def create_robot(num)
+    num.times do |n|
+      RobotWorld.create({:name          => "Bob#{n}",
+                         :city          => "Denver#{n}",
+                         :state         => "CO#{n}",
+                         :avatar        => "Narwhal#{n}",
+                         :birthdate     => "04/04/2014#{n}",
+                         :date_hired    => "05/05/2015#{n}",
+                         :department    => "domination#{n}"})
+    end
+  end
+
+  def test_user_can_edit_and_see_edited_robots
+    create_robot(1)
+
+    visit '/robots'
+
+    click_link("Reprogram")
+    assert_equal '/robots/1/edit', current_path
+    fill_in('robot[name]', with: "Frank")
+    fill_in('robot[city]', with: "Atlanta")
+    select "Colorado", :from => "robot[state]"
+    # find('robot[state]').find(:xpath, 'option[6]').select_option
+    fill_in('robot[avatar]', with: "Narwhal")
+    fill_in('robot[birthdate]', with: "04/04/2004")
+    fill_in('robot[date_hired]', with: "05/05/2005")
+    choose('domination')
+    click_button('submit')
+
+
+
+    within('#robo-data') do
+      assert page.has_content?("Frank")
+    end
+  end
+end
