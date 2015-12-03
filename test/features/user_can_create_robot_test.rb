eb82709e9527fc7eeb1f35a35e3ce6acae07b1d3
Dan Winter <dan.j.winter@gmail.com>
2015-12-02 21:10:52 -0700

    added feature test for user creating robot

--- test/features/user_can_create_robot_test.rb
+++ test/features/user_can_create_robot_test.rb
@@ -1,1 +1,25 @@
 require_relative '../test_helper'
+
+class UserCanCreateRobot < FeatureTest
+  def test_user_can_create_new_robot
+
+    visit '/robots/new'
+
+    fill_in('robot[name]', with: "Bob")
+    fill_in('robot[city]', with: "Denver")
+    select "Colorado", :from => "robot[state]"
+    # find('robot[state]').find(:xpath, 'option[6]').select_option
+    fill_in('robot[avatar]', with: "Narwhal")
+    fill_in('robot[birthdate]', with: "04/04/2004")
+    fill_in('robot[date_hired]', with: "05/05/2005")
+    choose('domination')
+    click_button('submit')
+
+    assert_equal '/robots', current_path
+
+    within('#robot-title-1') do
+      assert page.has_content?("Bob")
+      assert page.has_content?("Secret Agent 1")
+    end
+  end
+end
